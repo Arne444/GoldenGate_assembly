3f537cb1859800510cfc30f56aba0cbb92c6cdbc
Arne444 <32069544+Arne444@users.noreply.github.com>
2017-09-25 17:59:20 +0100

    Edit containers

--- GoldenGate.py
+++ GoldenGate.py
@@ -3,14 +3,14 @@
 from opentrons import robot, containers, instruments
 
 p20rack = containers.load('tiprack-10ul-H', 'B2', 'p20_rack')
-tube_rack = continers.load('tube-rack-2ml', 'A1', 'tube_rack')
+tube_rack = containers.load('tube-rack-2ml', 'D2', 'tube_rack')
 
 output = containers.load('96-PCR-flat', 'C1', 'output')
-trash = containers.load('trash-box', 'C3')
+trash = containers.load('trash-box', 'A3')
 
 p20 = instruments.Pipette(
 	tip_racks=[p20rack],
-	trash_containers=trash,
+	trash_container=trash,
 	min_volume = 2,
 	max_volume = 20,
 	axis="a"
@@ -22,33 +22,36 @@ GGmix = tube_rack.wells('A3')
 
 total_volume = 20
 DNA_volumes = [2, 3, 3.5, 4, 5, 6, 6.7, 7]
+num_assemblies = len(DNA_volumes)
 
-water_volumes []
+water_volumes = []
 for v in DNA_volumes:
 	water_volumes.append(total_volume - v - 4 - 2)
 
 p20.distribute(
 	water_volumes,
 	water_source,
-	output.wells('A1', length=DNA_volumes),
+	output.wells('A1', length=num_assemblies),
 	blow_out=True,
-	touch_tip=True
+	touch_tip=True,
 )
 
 p20.distribute(
 	4,
 	GGbuffer,
-	output.wells('A1', length=DNA_volumes),
-	mix_after(4, 3),
+	output.wells('A1', length=num_assemblies),
+	mix_after = (4, 3),
 	blow_out=True,
-	touch_tip=True
+	touch_tip=True,
+	new_tip='always'
 )
 
 p20.distribute(
 	2,
 	GGbuffer,
-	output.wells('A1', length=DNA_volumes),
-	mix_after(18, 5),
+	output.wells('A1', length=num_assemblies),
+	mix_after = (18, 5),
 	blow_out=True,
-	touch_tip=True
+	touch_tip=True,
+	new_tip='always'
 )
