48509355ff8006df1d7a385dca3466632809badc
Arne444 <32069544+Arne444@users.noreply.github.com>
2017-09-26 11:30:56 +0100

    Edit add custom tip rack and cool rack

--- GoldenGate.py
+++ GoldenGate.py
@@ -2,8 +2,19 @@
 
 from opentrons import robot, containers, instruments
 
-p20rack = containers.load('tiprack-10ul-H', 'B2', 'p20_rack')
-tube_rack = containers.load('tube-rack-2ml', 'D2', 'tube_rack')
+#Create 6x12 p20 tip rack container
+containers.create(
+'6x12_tiprack',
+grid=(6,12),
+spacing=(9, 9),
+diameter=5,
+depth=60
+)
+
+#Specify containers
+p20rack = containers.load('6x12_tiprack', 'B2', 'p20_rack')
+tube_rack = containers.load('tube-rack-2ml', 'D1', 'tube_rack')
+cool_rack = containers.load('tube-rack-2ml', 'E3', 'cool_rack')
 
 output = containers.load('96-PCR-flat', 'C1', 'output')
 trash = containers.load('trash-box', 'A3')
@@ -18,39 +29,39 @@ p20 = instruments.Pipette(
 
 water_source = tube_rack.wells('A1')
 GGbuffer = tube_rack.wells('A2')
-GGmix = tube_rack.wells('A3')
+GGmix = cool_rack.wells('A1')
 
 total_volume = 20
-DNA_volumes = [2, 3, 3.5, 4, 5, 6, 6.7, 7]
+DNA_volumes = [2, 3, 3.5, 4, 5, 6, 7, 8]
 num_assemblies = len(DNA_volumes)
 
 water_volumes = []
 for v in DNA_volumes:
 	water_volumes.append(total_volume - v - 4 - 2)
 
-p20.distribute(
-	water_volumes,
-	water_source,
-	output.wells('A1', length=num_assemblies),
-	blow_out=True,
-	touch_tip=True,
+p20.transfer(
+    water_volumes,
+    water_source,
+    output.wells('A1', length=num_assemblies),
+    blow_out=True,
+    touch_tip=True
 )
 
-p20.distribute(
+p20.transfer(
 	4,
 	GGbuffer,
 	output.wells('A1', length=num_assemblies),
-	mix_after = (4, 3),
+	mix_after = (3, 4),
 	blow_out=True,
 	touch_tip=True,
 	new_tip='always'
 )
 
-p20.distribute(
+p20.transfer(
 	2,
-	GGbuffer,
+	GGmix,
 	output.wells('A1', length=num_assemblies),
-	mix_after = (18, 5),
+	mix_after = (5, 18),
 	blow_out=True,
 	touch_tip=True,
 	new_tip='always'
