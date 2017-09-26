b057cf3c2f54563f49dbadafec55251324ac103f
Arne444 <32069544+Arne444@users.noreply.github.com>
2017-09-26 15:35:10 +0100

    Edit container positions

--- GoldenGate.py
+++ GoldenGate.py
@@ -2,45 +2,50 @@
 
 from opentrons import robot, containers, instruments
 
-#Create 6x12 p20 tip rack container
-containers.create(
-'6x12-tiprack',
-grid=(6,12),
-spacing=(9, 9),
-diameter=5,
-depth=60
-)
-
-#Create 3x6 2ml tube rack for DNA samples
-containers.create(
-'3x6-tube-rack-2ml',
-grid=(3,6),
-spacing=(19.5,19.5),
-diameter=9.5,
-depth=40
-)
-
 #Specify containers
-p20rack = containers.load('6x12-tiprack', 'B2', 'p20_rack')
 tube_rack = containers.load('tube-rack-2ml', 'D1', 'tube_rack')
 cool_rack = containers.load('tube-rack-2ml', 'E3', 'cool_rack')
-DNA_rack = containers.load('3x6-tube-rack-2ml', 'C3', 'DNA_rack')
 
 output = containers.load('96-PCR-flat', 'C1', 'output')
 trash = containers.load('trash-box', 'A3')
 
+#Create 6x12 p20 tip rack container
+containers.create(
+	'tiprack-200ul-6x12',
+	grid=(6,12),
+	spacing=(9, 9),
+	diameter=5,
+	depth=60
+)
+
+p20_rack = containers.load('tiprack-200ul-6x12', 'B2', 'p20_rack')
+
+#Create 3x6 2ml tube rack for DNA samples
+containers.create(
+	'3x6-tube-rack-2ml',
+	grid=(3,6),
+	spacing=(19.5,19.5),
+	diameter=9.5,
+	depth=40
+)
+
+DNA_rack = containers.load('3x6-tube-rack-2ml', 'C3', 'DNA_rack')
+
+#Specify p20 pipette
 p20 = instruments.Pipette(
-	tip_racks=[p20rack],
+	tip_racks=[p20_rack],
 	trash_container=trash,
 	min_volume = 2,
 	max_volume = 20,
 	axis="a"
 )
 
+#Define sources
 water_source = tube_rack.wells('A1')
 GGbuffer = tube_rack.wells('A2')
 GGmix = cool_rack.wells('A1')
 
+#Define DNA and water volumes to load
 total_volume = 20
 DNA_volumes = [2, 3, 3.5, 4, 5, 6, 7, 8]
 num_assemblies = len(DNA_volumes)
@@ -49,6 +54,7 @@ water_volumes = []
 for v in DNA_volumes:
 	water_volumes.append(total_volume - v - 4 - 2)
 
+#Add H2O
 p20.transfer(
     water_volumes,
     water_source,
@@ -57,6 +63,7 @@ p20.transfer(
     touch_tip=True
 )
 
+#Add GoldenGate buffer
 p20.transfer(
 	4,
 	GGbuffer,
@@ -67,6 +74,7 @@ p20.transfer(
 	new_tip='always'
 )
 
+#Add DNA
 p20.transfer(
 	DNA_volumes,
 	DNA_rack.wells('A1', length=num_assemblies),
@@ -77,6 +85,7 @@ p20.transfer(
 	new_tip='always'
 )
 
+#Add GoldenGate reaction mix
 p20.transfer(
 	2,
 	GGmix,
